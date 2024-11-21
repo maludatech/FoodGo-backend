11e8b14c1c98739f5f0120a87714a9c2b9d0499c
maluda <nwitevictor7@gmail.com>
2024-11-21 19:36:02 +0100

    utils/database.ts

---
+++ models/User.ts
@@ -0,0 +1,28 @@
+import { Schema, model, models } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: [true, "Fullname is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+    },
+    imageUrl: {
+      type: String,
+      required: [true, "ImageUrl is required"],
+    },
+    deliveryAddress: {
+      type: String,
+      required: false,
+      default: null,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = models.User || model("User", userSchema);
+
+export default User;
