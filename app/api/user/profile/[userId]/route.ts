4b1c0d7dcc38f3e455dc76385fbc452dfa8daf2a
maluda <nwitevictor7@gmail.com>
2024-12-03 02:07:17 +0100

    updated profile backend

--- app/api/user/profile/[userId]/route.ts
+++ app/api/user/profile/[userId]/route.ts
@@ -2,34 +2,6 @@ import { connectToDb } from "@/utils/database";
 import User from "@/models/User";
 import bcrypt from "bcrypt";
 
-// GET request to fetch user details by userId
-export const GET = async (
-  req: Request,
-  { params }: { params: { userId: string } }
-) => {
-  const userId = params.userId;
-  try {
-    await connectToDb();
-
-    const user = await User.findById(userId);
-
-    if (!user) {
-      console.error("User not found");
-      return new Response(JSON.stringify({ message: "User not found" }), {
-        status: 404,
-      });
-    }
-
-    return new Response(JSON.stringify(user), { status: 200 });
-  } catch (error) {
-    console.error("Error fetching user details", error);
-    return new Response(
-      JSON.stringify({ message: "Error fetching user details" }),
-      { status: 500 }
-    );
-  }
-};
-
 // PATCH request to update user details
 export const PATCH = async (
   req: Request,
@@ -42,22 +14,7 @@ export const PATCH = async (
 
     await connectToDb();
 
-    let updateData: Record<string, any> = {
-      ...(fullName && { fullName }),
-      ...(imageUrl && { imageUrl }),
-      ...(deliveryAddress && { deliveryAddress }),
-      ...(phoneNumber && { phoneNumber }),
-    };
-
-    // Hash the password if it is provided
-    if (password) {
-      const hashedPassword = await bcrypt.hash(password, 10);
-      updateData.password = hashedPassword;
-    }
-
-    const user = await User.findByIdAndUpdate(userId, updateData, {
-      new: true,
-    });
+    const user = await User.findById(userId);
 
     if (!user) {
       return new Response(JSON.stringify({ message: "User not found" }), {
@@ -65,10 +22,37 @@ export const PATCH = async (
       });
     }
 
+    let updateData: Record<string, any> = {
+      ...(fullName && { fullName }),
+      ...(imageUrl && { imageUrl }),
+      ...(deliveryAddress && { deliveryAddress }),
+      ...(phoneNumber && { phoneNumber }),
+    };
+
+    // Handle password update
+    if (password) {
+      const isSamePassword = await bcrypt.compare(password, user.password);
+      if (isSamePassword) {
+        return new Response(
+          JSON.stringify({
+            message: "New password cannot be the same as the old password.",
+          }),
+          { status: 400 }
+        );
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+      updateData.password = hashedPassword;
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(userId, updateData, {
+      new: true,
+    });
+
     return new Response(
       JSON.stringify({
         message: "User details updated successfully!",
-        user,
+        user: updatedUser,
       }),
       { status: 200 }
     );
