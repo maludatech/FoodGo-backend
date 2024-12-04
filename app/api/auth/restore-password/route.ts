c9775866464687d4a4af3a6e9cbb163f2c4b0634
maluda <nwitevictor7@gmail.com>
2024-12-04 18:14:44 +0100

    almsot done with auth backend

--- app/api/auth/restore-password/route.ts
+++ app/api/auth/restore-password/route.ts
@@ -3,5 +3,25 @@ import User from "@/models/User";
 
 export const POST = async (req: Request, res: Response) => {
   try {
-  } catch (error) {}
+    const code = await req.json();
+
+    await connectToDb();
+    const user = await User.findOne({ resetToken: code });
+
+    if (!user) {
+      return new Response(JSON.stringify({ message: "Code incorrect" }), {
+        status: 404,
+      });
+    }
+
+    return new Response(
+      JSON.stringify({ message: "Code correct", userId: user._id }),
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error resolving code: ", error);
+    return new Response(JSON.stringify({ message: "Internal server error" }), {
+      status: 500,
+    });
+  }
 };
