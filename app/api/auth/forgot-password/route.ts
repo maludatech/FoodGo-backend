01e5ce4b6b53850fea91733cdf32821780a5f329
maluda <nwitevictor7@gmail.com>
2024-12-04 17:24:34 +0100

    forgot password tested

--- app/api/auth/forgot-password/route.ts
+++ app/api/auth/forgot-password/route.ts
@@ -46,13 +46,13 @@ export const POST = async (req: Request, res: Response) => {
                 <div style="text-align: center;">
                     <img src="https://res.cloudinary.com/dlnvweuhv/image/upload/v1732232273/hamburger-burger-svgrepo-com_oevwg5.png" alt="FoodGo Logo" style="width: 80px; height: 80px; margin-bottom: 20px;" />
                 </div>
-                <p style="color: #333; font-size: 18px; font-weight: bold;">Hello ${user.username},</p>
+                <p style="color: #333; font-size: 18px; font-weight: bold;">Hello ${user.fullName},</p>
                 <p style="color: #555; font-size: 16px;">You recently requested to reset your password for your CryptFX account.</p>
                 <p style="color: #333; font-weight: bold; font-size: 22px; background-color: #f8f8f8; padding: 10px 15px; border-radius: 5px; text-align: center; margin: 20px 0;">
                     ${resetToken}
                 </p>
                 <p style="color: #555; font-size: 16px;">Please use the above code to reset your password. This code will expire in 30 minutes.</p>
-                <p style="color: #555; font-size: 16px;">If you did not request this, please ignore this email or <a href="https://CryptFX.vercel.app/contact" style="color: #EF2A39; text-decoration: none;">contact our support team</a> immediately.</p>
+                <p style="color: #555; font-size: 16px;">If you did not request this, please ignore this email.</p>
                 <hr style="border: none; border-top: 1px solid #eee; margin: 20px 0;" />
                 <p style="color: #555; font-size: 14px; text-align: center;">
                     Thank you for using <span style="color: #EF2A39; font-weight: bold;">FoodGo</span>.<br>
